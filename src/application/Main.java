7773231e42dd0a04074fdc3bb7d5109084c05e33
Swellington Soares <swellington.santos@gmail.com>
2023-07-31 16:50:25 -0300

    Criação das classe UI, Piece, Color, ChessPiece

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,13 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Position position = new Position(3,5);
-        System.out.println(position);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
+
     }
 }
---
+++ src/boardgame/Piece.java
@@ -0,0 +1,13 @@
+package boardgame;
+
+public abstract class Piece {
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+    protected Board getBoard() {
+        return board;
+    }
+}
