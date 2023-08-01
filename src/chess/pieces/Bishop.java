74adb352820ba56bba4ecbd86175733972fbae5c
Swellington Soares <swellington.santos@gmail.com>
2023-08-01 15:26:36 -0300

    Movimento do Bispo

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -18,6 +18,45 @@ public class Bishop extends ChessPiece {
 
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[0][];
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        Position p = new Position(0, 0);
+
+        //nw
+        p.setValues(position.getRow() - 1, position.getCol() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+            p.setValues(p.getRow() - 1, p.getCol() - 1);
+        }
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) mat[p.getRow()][p.getCol()] = true;
+
+        //ne
+        p.setValues(position.getRow() - 1, position.getCol() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+            p.setValues(p.getRow() - 1, p.getCol() + 1);
+        }
+
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) mat[p.getRow()][p.getCol()] = true;
+
+        //se
+        p.setValues(position.getRow() + 1, position.getCol() + 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+            p.setValues(p.getRow() + 1, p.getCol() + 1);
+        }
+
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p)) mat[p.getRow()][p.getCol()] = true;
+
+        //sw
+        p.setValues(position.getRow() + 1, position.getCol() - 1);
+        while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+            mat[p.getRow()][p.getCol()] = true;
+            p.setValues(p.getRow() + 1, p.getCol() - 1);
+        }
+
+        if (getBoard().positionExists(p) && isThereOpponentPiece(p))
+            mat[p.getRow()][p.getCol()] = true;
+
+        return mat;
     }
 }
